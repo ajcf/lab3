78ed56c1f963fa40d3a8e82b4c91dda08f9afe97
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2012-11-25 10:45:42 -0500

    Initialized git

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package lab3;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * An example remote interface for RMI. RMI interfaces must extend the base Remote interface.
+ */
+public interface Hello extends Remote {
+
+  String sayHello() throws RemoteException;
+
+}
+
